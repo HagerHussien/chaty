05840aa079ec7b81fd59ab8a06bc1e552d810ef4
baghdadino <baghdadinoo@gmail.com>
2017-05-07 20:40:10 +0200

    adding more templates

--- www/js/routes.js
+++ www/js/routes.js
@@ -1,6 +1,40 @@
 //js/routes.js
 angular.module('chaty').config(function($stateProvider){
 $stateProvider
+  .state('app',{
+    url:'/app',
+    templateUrl:"templates/app.html",
+    abstract:true
+  }).state('app.about',{
+    url:'/about',
+    views:{
+      "pageContent":{
+        templateUrl:"templates/about.html"
+      }
+    }
+  }).state('app.activeusers',{
+    url:'/activeusers',
+    views:{
+      "pageContent":{
+        templateUrl:"templates/activeusers.html"
+      }
+    }
+  }).state('app.chatroom',{
+    url:'/chatroom',
+    views:{
+      "pageContent":{
+        templateUrl:"templates/chatroom.html"
+      }
+    }
+  }).state('app.privatechat',{
+    url:'/privatechat',
+    views:{
+      "pageContent":{
+        templateUrl:"templates/privatechat.html"
+      }
+    }
+  })
+
   .state('basic',{
     url:'/basic',
     templateUrl:"templates/basic.html",
